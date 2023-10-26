9169107b2e7f8893b94ac0ca19b96adf07c2aa05
ddkktt <128318872+ddkktt@users.noreply.github.com>
2023-10-25 20:49:13 -0600

    Add files via upload

---
+++ t3/t3 - Copy/src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl: string = 'localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(userData: any): Observable<any> {
+    const url = `${this.baseUrl}/auth/register`;
+    return this.http.post(url, userData);
+  }
+}
